c4cfbc31cf9bc937f853ceba62ef30c883d15dbe
ImJustNon <Im_Just_Non@hotmail.com>
2026-02-20 00:42:22 +0700

    feat: add admin client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react"
 import { usernameClient } from "better-auth/client/plugins"
+import { adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     plugins: [
-        usernameClient()
+        usernameClient(),
+        adminClient()
     ]
 })
