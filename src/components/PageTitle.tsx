81de87cee747f7c8c0b0e384b66a529cec80f63a
ImJustNon <Im_Just_Non@hotmail.com>
2026-02-20 16:01:11 +0700

    refactor: move title to compoennt

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PageTitle({ title, description }: { title: string; description: string }): React.JSX.Element {
+    return(
+        <>
+            <div className="mb-8">
+                <h1 className="text-2xl font-bold text-foreground">{title}</h1>
+                <p className="text-sm text-muted-foreground">{description}</p>
+            </div>
+        </>
+    );
+}
