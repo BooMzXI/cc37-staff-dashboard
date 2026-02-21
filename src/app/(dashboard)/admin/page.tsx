05d6418c573bce7e008e53d3602e0243fd270077
ImPatipol <122760030+BooMzXI@users.noreply.github.com>
2026-02-21 20:15:22 +0700

    feat: add Radix UI dialog component and enhance admin page layout with title and description

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -3,6 +3,18 @@
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import PageTitle from "@/components/PageTitle";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Card, CardContent } from "@/components/ui/card";
 
 type User = {
   role?: string;
@@ -14,10 +26,20 @@ export default function AdminPage() {
 
   useEffect(() => {
     if (session && (session.user as User)?.role !== 'admin') {
-      router.push("/"); // ถ้าไม่ใช่ admin ดีดกลับหน้าแรก
+      router.push("/");
     }
   }, [session, router]);
 
 
-  return <div>Welcome Admin!</div>
+  return (
+    <div className="min-h-screen bg-background">
+      <main className="mx-auto max-w-7xl px-4 py-8 lg:px-6">
+        <PageTitle 
+          title="Admin Dashboard"
+          description="Welcome Administrator! Here you can manage the system settings and user accounts."
+        />
+        <div className="w-full flex items-end">ff</div>
+      </main>
+    </div>
+  )
 }
