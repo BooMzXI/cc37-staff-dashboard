9a5fbcdae693d3382697d50cf6445326f7a49df5
ImJustNon <Im_Just_Non@hotmail.com>
2026-02-23 21:39:02 +0700

    chore: package version

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -9,64 +9,43 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
 import PageTitle from "@/components/PageTitle";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { Bolt, List, Settings2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Bolt, List, PenLine, Settings2, Trash } from "lucide-react";
+import axios from "axios";
+import { config } from "@/config/config";
 
 type User = {
 	role?: string;
 };
 
-const invoices = [
-	{
-		invoice: "INV001",
-		paymentStatus: "Paid",
-		totalAmount: "$250.00",
-		paymentMethod: "Credit Card",
-	},
-	{
-		invoice: "INV002",
-		paymentStatus: "Pending",
-		totalAmount: "$150.00",
-		paymentMethod: "PayPal",
-	},
-	{
-		invoice: "INV003",
-		paymentStatus: "Unpaid",
-		totalAmount: "$350.00",
-		paymentMethod: "Bank Transfer",
-	},
-	{
-		invoice: "INV004",
-		paymentStatus: "Paid",
-		totalAmount: "$450.00",
-		paymentMethod: "Credit Card",
-	},
-	{
-		invoice: "INV005",
-		paymentStatus: "Paid",
-		totalAmount: "$550.00",
-		paymentMethod: "PayPal",
-	},
-	{
-		invoice: "INV006",
-		paymentStatus: "Pending",
-		totalAmount: "$200.00",
-		paymentMethod: "Bank Transfer",
-	},
-	{
-		invoice: "INV007",
-		paymentStatus: "Unpaid",
-		totalAmount: "$300.00",
-		paymentMethod: "Credit Card",
-	},
-];
-
 export default function AdminPage() {
 	const { data: session } = authClient.useSession();
 	const router = useRouter();
+	const [staffAccouts, setStaffAccount] = useState([]);
+
+	if (session?.user && (session.user as User)?.role !== "admin") return null;
 
 	useEffect(() => {
 		if (session && (session.user as User)?.role !== "admin") {
@@ -74,9 +53,15 @@ export default function AdminPage() {
 		}
 	}, [session, router]);
 
-	// use
+	useEffect(() =>{
+		(async() => {
+			axios.defaults.withCredentials = true;
+			const staffAccount = await axios.get(`${config.backend.baseUrl}/api/staff/account`);
+			setStaffAccount(staffAccount.data);
+		})();
+	}, []);
 
-	if (session?.user && (session.user as User)?.role !== "admin") return null;
+	
 
 	return (
 		<>
@@ -84,9 +69,8 @@ export default function AdminPage() {
 				title="Admin Panel"
 				description="หน้าต่างจัดการระบบสำหรับ Admin"
 			/>
-			<div className="text-2xl font-bold">Staff Accounts</div>
+			<div className="text-2xl font-bold mb-5">Staff Accounts</div>
 			<Table>
-				<TableCaption>A list of your recent invoices.</TableCaption>
 				<TableHeader>
 					<TableRow>
 						<TableHead>Username</TableHead>
@@ -97,25 +81,57 @@ export default function AdminPage() {
 					</TableRow>
 				</TableHeader>
 				<TableBody>
-					{invoices.map((invoice) => (
-						<TableRow key={invoice.invoice}>
-							<TableCell>{invoice.invoice}</TableCell>
-							<TableCell>{invoice.paymentStatus}</TableCell>
-							<TableCell>{invoice.paymentMethod}</TableCell>
-							<TableCell>{invoice.totalAmount}</TableCell>
+					{staffAccouts.map((ac: any, i: number) => (
+						<TableRow key={i}>
+							<TableCell>{ac.username}</TableCell>
+							<TableCell>{ac.name}</TableCell>
+							<TableCell>{ac.email}</TableCell>
+							<TableCell>{ac.role}</TableCell>
 							<TableCell>
-								<div className="p-2 bg-white text-black w-fit rounded-md hover:scale-105 active:scale-95 duration-300 cursor-pointer"><Settings2 strokeWidth={3} size={20} /></div>
+								<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<div className="p-2 bg-white text-black w-fit rounded-md hover:scale-105 active:scale-95 duration-300 cursor-pointer"><Settings2 strokeWidth={3} size={20} /></div>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent>
+									<DropdownMenuGroup>
+									<DropdownMenuLabel>{ac.username}</DropdownMenuLabel>
+									</DropdownMenuGroup>
+									<DropdownMenuSeparator />
+									<DropdownMenuItem><PenLine />Edit</DropdownMenuItem>
+									<DropdownMenuItem><Trash />Remove</DropdownMenuItem>
+								</DropdownMenuContent>
+								</DropdownMenu>
+								
 							</TableCell>
 						</TableRow>
 					))}
 				</TableBody>
-				<TableFooter>
-					<TableRow>
-						<TableCell colSpan={3}>Total</TableCell>
-						<TableCell className="text-right">$2,500.00</TableCell>
-					</TableRow>
-				</TableFooter>
 			</Table>
+
+			<Pagination className="mt-10">
+				<PaginationContent>
+					<PaginationItem>
+						<PaginationPrevious href="#" />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">1</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#" isActive>
+							2
+						</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">3</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationNext href="#" />
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
 		</>
 	);
 }
