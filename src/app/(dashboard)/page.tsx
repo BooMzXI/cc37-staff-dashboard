5b318b06bbe526f7be9d573dba34a9bca61bf8ae
ImPatipol <122760030+BooMzXI@users.noreply.github.com>
2026-02-23 21:37:24 +0700

    fix: change api path

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -20,7 +20,7 @@ export default function Dashboard() {
 		const fetchStats = async () => {
 			try {
 				const res = await fetch(
-					`${process.env.NEXT_PUBLIC_API_URL}/api/statistic`,
+					`/api/statistic`,
 					{
 						credentials: "include",
 					},
