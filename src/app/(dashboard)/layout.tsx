4653623096d81f64ca43f113ee45b8bd55578373
ImJustNon <Im_Just_Non@hotmail.com>
2026-02-28 15:22:23 +0700

    fix: ?

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ async function DashboardShell({ children }: { children: React.ReactNode }) {
 	});
 	return (
 		<>
-			<DashboardNavbar role={data?.user.role} />
+			<DashboardNavbar role={data?.user?.role} />
 			{children}
 		</>
 	);
