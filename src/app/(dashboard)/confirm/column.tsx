e5fd0a7884d666c729ebffec0bfd548aa54340f3
ImPatipol <122760030+BooMzXI@users.noreply.github.com>
2026-03-16 22:54:45 +0700

    feat: implement mock data generation and update confirmation page with tabbed display

--- src/app/(dashboard)/confirm/column.tsx
+++ src/app/(dashboard)/confirm/column.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, Pin, Search } from "lucide-react";
+import { ArrowUpDown, Pin, Search, Send } from "lucide-react";
 import Link from "next/link";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
@@ -18,32 +19,11 @@ export interface StudentConfirmation {
 		std_info_nick_name: string;
 		std_info_first_name: string;
 		std_info_last_name: string;
+		std_info_education_level: string;
 	};
 }
 
 export const columns: ColumnDef<StudentConfirmation>[] = [
-	{
-		accessorKey: "std_user.email",
-		header: ({ column }) => {
-			return (
-				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Email <ArrowUpDown className="ml-2 h-4 w-4" />
-				</Button>
-			);
-		},
-		cell: ({ row }) => {
-			const email = row.original.std_user?.email || "-";
-			return (
-				<div className="w-[180px] md:w-[200px] lg:w-[250px] truncate" title={email}>
-					{email}
-				</div>
-			);
-		},
-	},
-	{
-		accessorKey: "std_info.std_info_phone_number",
-		header: "เบอร์โทรศัพท์",
-	},
 	{
 		accessorKey: "std_info",
 		header: ({ column }) => {
@@ -90,6 +70,39 @@ export const columns: ColumnDef<StudentConfirmation>[] = [
 		cell: ({ row }) => (row.original.std_info?.std_info_gender === "male" ? "ชาย" : "หญิง"),
 	},
 	{
+		accessorKey: "std_info.std_info_education_level",
+		header: "ระดับชั้น",
+		cell: ({ row }) => {
+			const level = row.original.std_info?.std_info_education_level || "-";
+			const flevel = decodeURI(level);
+			if (flevel.includes("4")) return "ม.4";
+			if (flevel.includes("5")) return "ม.5";
+			return flevel;
+		},
+	},
+	{
+		accessorKey: "std_user.email",
+		header: ({ column }) => {
+			return (
+				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+					Email <ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			);
+		},
+		cell: ({ row }) => {
+			const email = row.original.std_user?.email || "-";
+			return (
+				<div className="w-[180px] md:w-[200px] lg:w-[250px] truncate" title={email}>
+					{email}
+				</div>
+			);
+		},
+	},
+	{
+		accessorKey: "std_info.std_info_phone_number",
+		header: "เบอร์โทรศัพท์",
+	},
+	{
 		id: "actions",
 		cell: ({ row }) => {
 			const application = row.original;
@@ -97,21 +110,27 @@ export const columns: ColumnDef<StudentConfirmation>[] = [
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<Button variant="outline">
-							<Search className="h-4 w-4" />
+							<Send className="h-4 w-4" />
 						</Button>
 					</DropdownMenuTrigger>
 					<DropdownMenuContent>
 						<DropdownMenuGroup>
 							<DropdownMenuLabel>{`ตรวจสอบ ${application.std_user.name}`}</DropdownMenuLabel>
 							<DropdownMenuSeparator />
-							<DropdownMenuItem>
-								<Link href={`/confirm/${application.std_application_id}`} className="flex items-center">
-									<Pin className="mr-2" />
-									<p>ยืนยันสิทธิ์</p>
-								</Link>
+							<DropdownMenuItem
+								onClick={() => {
+									toast.success("ยืนยันสิทธิ์สำเร็จ");
+								}}
+								className="flex items-center cursor-pointer"
+							>
+								<Pin className="mr-2" />
+								<p>ยืนยันสิทธิ์</p>
 							</DropdownMenuItem>
 							<DropdownMenuItem>
-								<Link href={`/personal-information/${application.std_application_id}`}>ข้อมูลส่วนตัว</Link>
+								<Link href={`/personal-information/${application.std_application_id}`} className="flex items-center">
+									<Search className="mr-2" />
+									<p>ข้อมูลส่วนตัว</p>
+								</Link>
 							</DropdownMenuItem>
 						</DropdownMenuGroup>
 					</DropdownMenuContent>
--- src/app/(dashboard)/confirm/page.tsx
+++ src/app/(dashboard)/confirm/page.tsx
@@ -5,42 +5,86 @@ import React, { useEffect, useState } from "react";
 import { DataTable } from "@/components/DataTable";
 import Loading from "@/components/Loading";
 import PageTitle from "@/components/PageTitle";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { config } from "@/config/config";
 import { columns, StudentConfirmation } from "./column";
+import { generateMockData } from "./mock/mockData";
 
-export default function PersonalInformation() {
-	const [loading, setLoading] = useState(true);
+export default function PersonalConfirmation() {
+	const [loading, setLoading] = useState(true); // อย่าลืมเปลี่ยนเป็น true หลังเทส
 	const [data, setData] = useState<StudentConfirmation[]>([]);
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await fetch(`${config.backend.baseUrl}/api/staff/application/all`, {
-					method: "GET",
-					credentials: "include",
-				});
-				if (!res.ok) throw new Error("Failed to fetch data");
-				const jsonData = await res.json();
-				setData(jsonData);
-			} catch (error) {
-				console.error("Failed to fetch data:", error);
-			} finally {
+		const fetchMockData = () => {
+			setTimeout(() => {
+				const mockStudents = generateMockData();
+				setData(mockStudents);
 				setLoading(false);
-			}
+			}, 1000);
 		};
 
-		fetchData();
+		fetchMockData();
 	}, []);
 
-	if (loading) {
-		return <Loading />;
-	}
+	/*useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(``, {
+                    method: "GET",
+                    credentials: "include",
+                });
+                
+                if (!res.ok) throw new Error("Failed to fetch data");
+                const jsonData = await res.json();
+                setData(jsonData);
+            } catch (error) {
+                console.error("Failed to fetch data:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);*/
+
+	if (loading) return <Loading />;
+
+	const maleM4 = data.filter((s) => s.std_info?.std_info_gender === "male" && s.std_info?.std_info_education_level?.includes("4"));
+	const maleM5 = data.filter((s) => s.std_info?.std_info_gender === "male" && s.std_info?.std_info_education_level?.includes("5"));
+	const femaleM4 = data.filter((s) => s.std_info?.std_info_gender === "female" && s.std_info?.std_info_education_level?.includes("4"));
+	const femaleM5 = data.filter((s) => s.std_info?.std_info_gender === "female" && s.std_info?.std_info_education_level?.includes("5"));
 
 	return (
 		<>
-			<PageTitle title="Personal Confirmation" description={`การยืนยันใบสมัครของนักเรียน`} />
-			<div className="mt-6 rounded-lg shadow-sm">
-				<DataTable columns={columns} data={data} />
+			<PageTitle title="Personal Confirmation" description={`การประกาศผลและยืนยันสิทธิ์ของนักเรียน`} />
+
+			<div className="mt-6">
+				<Tabs defaultValue="male-m4" className="w-full">
+					<TabsList className="grid w-full grid-cols-2 md:grid-cols-4 mb-4">
+						<TabsTrigger value="male-m4">ชาย ม.4</TabsTrigger>
+						<TabsTrigger value="male-m5">ชาย ม.5</TabsTrigger>
+						<TabsTrigger value="female-m4">หญิง ม.4</TabsTrigger>
+						<TabsTrigger value="female-m5">หญิง ม.5</TabsTrigger>
+					</TabsList>
+
+					<div className="rounded-lg shadow-sm bg-card">
+						<TabsContent value="male-m4" className="mt-0">
+							<DataTable columns={columns} data={maleM4} />
+						</TabsContent>
+
+						<TabsContent value="male-m5" className="mt-0">
+							<DataTable columns={columns} data={maleM5} />
+						</TabsContent>
+
+						<TabsContent value="female-m4" className="mt-0">
+							<DataTable columns={columns} data={femaleM4} />
+						</TabsContent>
+
+						<TabsContent value="female-m5" className="mt-0">
+							<DataTable columns={columns} data={femaleM5} />
+						</TabsContent>
+					</div>
+				</Tabs>
 			</div>
 		</>
 	);
