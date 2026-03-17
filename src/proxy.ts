5973572e61d71ae0fc1a4bce67bf10b6670eba09
ImJustNon <Im_Just_Non@hotmail.com>
2026-03-18 03:48:10 +0700

    fix: check user permission every time

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,20 +1,33 @@
 import { headers } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { config as appConfig } from "./config/config";
 import { authClient } from "./lib/auth-client";
 
 export async function proxy(request: NextRequest) {
-	let sessionToken = request.cookies.get("better-auth.session_token");
-	if (appConfig.isProd) {
-		sessionToken = request.cookies.get("__Secure-better-auth.session_token");
-	}
+	// let sessionToken = request.cookies.get("better-auth.session_token");
+	// if (appConfig.isProd) {
+	// 	sessionToken = request.cookies.get("__Secure-better-auth.session_token");
+	// }
 
-	// const { data: sessionToken } = await authClient.getSession({
-	// 	fetchOptions: { headers: await headers() },
-	// });
+	const { data: sessionToken } = await authClient.getSession({
+		fetchOptions: { headers: await headers() },
+	});
 
 	const isLoginPage = request.nextUrl.pathname.startsWith("/login");
+
+	if (sessionToken?.user.role === "user") {
+		if (isLoginPage) {
+			const response = NextResponse.next();
+			response.cookies.delete("better-auth.session_token");
+			response.cookies.delete("__Secure-better-auth.session_token");
+			return response;
+		}
+
+		const response = NextResponse.redirect(new URL("/login", request.url));
+		response.cookies.delete("better-auth.session_token");
+		response.cookies.delete("__Secure-better-auth.session_token");
+		return response;
+	}
 
 	if (!sessionToken && !isLoginPage) {
 		return NextResponse.redirect(new URL("/login", request.url));
